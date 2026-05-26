8cdbd6674413697921a5e9a35ebfb9f7382be690
Eric Brown <eric@ebrown.net>
2026-05-26 02:56:20 +0000

    C3 hardware support updates

--- board/drivers/drivers.h
+++ board/drivers/drivers.h
@@ -46,7 +46,7 @@ extern uint32_t rx_buffer_overflow;
 
 extern can_health_t can_health[PANDA_CAN_CNT];
 
-// Ignition detected from CAN messages
+// Ignition detected from CAN meessages
 extern bool ignition_can;
 extern uint32_t ignition_can_cnt;
 
@@ -79,6 +79,7 @@ void can_set_orientation(bool flipped);
 #ifdef PANDA_JUNGLE
 void can_set_forwarding(uint8_t from, uint8_t to);
 #endif
+void ignition_can_hook(CANPacket_t *to_push);
 bool can_tx_check_min_slots_free(uint32_t min);
 uint8_t calculate_checksum(const uint8_t *dat, uint32_t len);
 void can_set_checksum(CANPacket_t *packet);
